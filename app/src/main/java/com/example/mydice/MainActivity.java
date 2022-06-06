71f79b5a83e751bd746ccc76a689ead3d1f587d9
Tommy Lee <tommylexlee@gmail.com>
2022-06-06 13:22:31 +0800

    1. Dice outcome will now be determined by the second value of the current time. Code logic is still a work in progress.

--- app/src/main/java/com/example/mydice/MainActivity.java
+++ app/src/main/java/com/example/mydice/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mydice;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -11,6 +13,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
@@ -32,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (MotionEvent.ACTION_UP == action) {
             Random rand = new Random();
-            rollDice(rand.nextInt(12) + 1);
+            rollDice();
             return true;
         }
 
@@ -42,9 +46,17 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Generates two random number, adds them together and check if the result is equals to the targetNum
      * If it is, then store the corresponding two random numbers in a list and return it
-     * @param targetNum
      */
-    private void rollDice(int targetNum) {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void rollDice() {
+
+        LocalDateTime date = LocalDateTime.now();
+        int curSec = date.toLocalTime().toSecondOfDay();
+        int timeValue = curSec % 12; //Generates a value from 0 - 11 (both inclusive)
+        int targetNum = timeValue + 1; //Adds 1 so that the max value attainable is 12 instead of 11
+        Log.d("rollDice", "timeValue: " + timeValue);
+        Log.d("rollDice", "curSec: " + curSec);
+        Log.d("rollDice", "targetNum: " + targetNum);
 
         Random rand = new Random();
         List<Integer> correctPair = new ArrayList<Integer>();
@@ -80,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
         Button b = (Button) view;
         Log.d("View", "beginRow: " + b.getText());
 
-        rollDice(Integer.valueOf(b.getText().toString()));
+        //rollDice(Integer.valueOf(b.getText().toString()));
     }
 
     private void setDiceImage(int num, ImageView iv) {
@@ -115,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
         Button b = (Button) view;
         Log.d("View", "beginRow: " + b.getText());
 
-        rollDice(Integer.valueOf(b.getText().toString()));
+        //rollDice(Integer.valueOf(b.getText().toString()));
 
         Log.d("toggleCheatMode", "toggleCheatMode: " + cheatMode);
     }
